5c3a323d6693f533c4a7cacc4be3be3665698713
lazukr <peterh10201@gmail.com>
2018-01-08 01:22:56 -0800

    updated nova sig command, added dice command, updated README to include acknowledgements

--- events/message.js
+++ events/message.js
@@ -1,22 +1,20 @@
-const logger = require('../logger.js')("message module");
+const logger = require('../logger.js')("Event Module: Message");
 module.exports = (discordBot, message) => {
   // ignore bots
   if (message.author.bot) return;
   // ignore all messages that does not begin
   // with the command prefix defined in the config
   if (message.content.indexOf(discordBot.config.commandPrefix) !== 0) return;
-
   const args = message.content.slice(discordBot.config.commandPrefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
-  logger.info(`${command}`);
   const cmd = discordBot.commands.get(command);
-
   if (!cmd) {
     logger.debug(`${cmd} was not loaded or not found.`);
     return;
   }
-
-  logger.info(`message-module: ${message.author.username}(${message.author.id}) ran command ${cmd.info.name} with arguments: ${args}`);
-  cmd.run(discordBot, message, args);
+  logger.info(`${message.author.username}(${message.author.id}) ran command ${cmd.info.name} with arguments: ${args}`);
+  cmd.run(discordBot, message, args)
+    .catch(err => {
+      console.log(err);
+    });
 };
