fe6bff75ada14ab5bde2ffa2a29ba506eec29e83
lazukr <peterh10201@gmail.com>
2018-01-23 22:03:22 -0800

    restructured code so that logger.js no longer needs relative pathing. refactored rochargenphp out into a separate function to be called by sig or char

---
+++ src/commands/nova/char.js
@@ -0,0 +1,18 @@
+const novaCharLink = 'https://www.novaragnarok.com/ROChargenPHP/character/';
+const logger = require('logger.js')("Nova Command module: char");
+const getCharaLink = require('ro-chargen-php-link.js');
+const ACTION_MAX = 25;
+const ROTATE_MAX = 7;
+const INVALID = -1;
+
+exports.run = async (discordBot, message, args) => {
+  logger.info(args);
+  getCharaLink(message, args, [ACTION_MAX, ROTATE_MAX], novaCharLink); 
+};
+
+exports.info = {
+  name: "char",
+  category: "Nova",
+  description: "displays a character for Nova RO characters. #/# indicates the action and rotation of the character respectively. Actions seems to go from 0 - 25, rotation goes from 0 - 7.",
+  usage: "<cmd_prefix>char #/#",
+};
