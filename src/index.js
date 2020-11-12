f442b1295637d5f3746bdd8587876b9689816309
lazukr <peterh10201@gmail.com>
2020-11-12 14:37:13 -0800

    added a catcha-all unhandled errors to hopefully help prevent bot from crashing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 require = require("esm")(module);
 module.exports = require("./main.js");
+
+process.on('unhandledRejection', error => {
+    console.log('Unhandled Promise rejection:', error);
+});
