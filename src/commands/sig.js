0e839f3625dc839c717e33da638c1e64c1861e9b
lazukr <peterh10201@gmail.com>
2021-06-06 15:45:20 -0700

    updated sig to 15

--- src/commands/sig.js
+++ src/commands/sig.js
@@ -2,7 +2,7 @@ import Logger from '../utils/logger';
 import Command from '../utils/command';
 import ROChargenPHP from '../utils/rochargenphp';
 
-export const MAX_BACKGROUND = 14;
+export const MAX_BACKGROUND = 15;
 export const MAX_POSE = 12;
 
 export default class Signature extends Command {
