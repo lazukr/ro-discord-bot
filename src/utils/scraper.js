fc5862319be7573582dab30b3b2c353d0713c5f4
lazukr <peterh10201@gmail.com>
2021-06-22 18:15:38 -0700

    revert session changes as captcha is back 1

--- src/utils/scraper.js
+++ src/utils/scraper.js
@@ -47,7 +47,7 @@ export default class Scraper {
         return 0;
       }
       */
-      Scraper.getPage = getPageWithCookie(undefined);
+      Scraper.getPage = getPageWithCookie(null);
       Logger.log('Logged in!');
       this.bot.adminChannel.send(`Logged in!`);
       //this.bot.scheduler.processQueues();
